110c0b42a681d9ec9c91614dc9d8e34abcc7f42e
k24dizzle <zhaok24@uw.edu>
2017-08-13 01:05:27 -0700

    Playing around with some db stuff

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # socks
 
-A stock game
+ Not a stock game
 
 1. Install Node (https://nodejs.org/en/download/)
 2. Run `npm install`
 3. Run `npm start`
+
--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert');
+const bodyParser = require('body-parser')
+
+// Connection URL
+// const url = 'mongodb://localhost:27017/myproject'; // Local
+const url = 'mongodb://kevin:test@ds127341.mlab.com:27341/socksdb'; // Hosted
+let database;
+
+// Apparently these two lines let you parse a request body
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.send('Mjork!')
+	res.sendfile('./public/index.html');
 })
 
+app.post('/test', function(req, res) {
+	console.log("Request made!");
+	console.log(req.body);
+	let collection = database.collection('test');
+	collection.insert({name: req.body.name_field});
+});
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+	database = db;
+	assert.equal(null, err);
+	console.log("Connected successfully to server");
+});
+
 app.listen(3000, function () {
-  console.log('Go to http://localhost:3000/')
+	console.log('Go to http://localhost:3000/')
 })
+
+// var insertDocuments = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Insert some documents
+//   collection.insertMany([
+//     {a : 1}, {a : 2}, {a : 3}
+//   ], function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(3, result.result.n);
+//     assert.equal(3, result.ops.length);
+//     console.log("Inserted 3 documents into the collection");
+//     callback(result);
+//   });
+// }
