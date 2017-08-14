4d9efdbb5ed7be16f214aa79fc27ce9b59d39665
k24dizzle <zhaok24@uw.edu>
2017-08-14 00:42:51 -0700

    get the 5k times yo

--- app/gatherIds.py
+++ app/gatherIds.py
@@ -21,10 +21,40 @@ def getAidsFromTeam(team_id):
 		href = male.find('a')['href']
 		# Example of an href = Athlete.aspx?AID=88448#tT1
 		# we just want the AID, which is between = and #
-		aid = href[(href.index('=') + 1):href.index('#')]
+		aid = find_between(href, "=", "#")
 		aids.append(aid)
 
 	return aids
 
 # 499 is skyline's team id
 # getAidsFromTeam(499)
+
+
+# TODO finish up this method, stopping because im tired
+def get5kTimes(aid):
+	url = "https://www.athletic.net/CrossCountry/Athlete.aspx?AID=" + str(aid) + "#/L0"
+	r = requests.get(url)
+	soup = BeautifulSoup(r.content, 'html.parser')
+	seasons = soup.find_all("div", {"class": "season"})
+
+	for season in seasons:
+		seasonId = season['id']
+		year = find_between(seasonId, "S-", "_")
+		# finding the 5k races for that season
+		header5k = season.find(string=re.compile("^5,000"))
+		races5k = header5k.find_next("table").find_all('tr')
+
+		times = list()
+		for race in races5k:
+			print race.find(href=re.compile("^/result/")).text
+
+
+def find_between(s, first, last):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+get5kTimes(88448)
