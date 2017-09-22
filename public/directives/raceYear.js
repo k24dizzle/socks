34baeaabeb7337ab2edda95321b30228e898593c
k24dizzle <zhaok24@uw.edu>
2017-09-22 15:17:07 -0700

    nice green tint for srs

--- public/directives/raceYear.js
+++ public/directives/raceYear.js
@@ -6,7 +6,22 @@ app.directive('raceYear', ['apiService', function(api) {
 			year: '=',
 		},
 		link: function(scope, element, attrs) {
+			let races = scope.year.races;
+			races.sort(function(a, b) {
+				return (a.time).localeCompare(b.time);
+			})
+
+			scope.slowest = races[races.length - 1].time;
+			scope.fastest = races[0].time;
 			
+			for (var i = 0; i < races.length; i++) {
+				races[i].rank = i;
+			}
+
+			races.sort(function(a, b) {
+				return (a.date).localeCompare(b.date)
+			})
+
 		},
 		templateUrl: '../templates/raceyear.html'
 	}
